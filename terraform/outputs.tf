e5db5cbe660abd7e01297e7633c2f58786509304
Patrick Heese <pheese8@gmail.com>
2025-08-18 19:35:55 -0400

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,9 +3,19 @@ output "lambda_function_name" {
   value       = aws_lambda_function.polly_narrator.function_name
 }
 
+output "lambda_function_arn" {
+  description = "The ARN of the Lambda function"
+  value       = aws_lambda_function.polly_narrator.arn
+}
+
 output "s3_bucket_name" {
   description = "The name of the S3 bucket"
   value       = aws_s3_bucket.polly_audio_bucket.bucket
+}
+
+output "s3_bucket_arn" {
+  description = "The ARN of the S3 bucket"
+  value       = aws_s3_bucket.polly_audio_bucket.arn
 }
 
 output "lambda_role_arn" {
