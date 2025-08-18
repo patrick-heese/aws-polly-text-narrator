2d640f0ddac9c22c1f48ffe9f86b1f2769d890ed
Patrick Heese <pheese8@gmail.com>
2025-08-18 19:35:10 -0400

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,12 +4,6 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-variable "s3_bucket_name" {
-  description = "Name for the S3 bucket to store Polly audio files"
-  type        = string
-  default     = "my-polly-audio-files-123456789012-us-east-1"  # Change this to your desired unique bucket name
-}
-
 variable "lambda_role_name" {
   description = "IAM Role name for the Lambda function"
   type        = string
